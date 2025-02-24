93b68ea38fce64bd7ce98a2a1b08f19a3358436d
natanael731 <cristofenatanael@gmail.com>
2025-02-24 11:07:20 -0300

    Add files via upload

--- PositNegatZero.java
+++ PositNegatZero.java
@@ -1,10 +1,25 @@
 package EstruturasCondicionais;
 
+import java.util.Scanner;
+
 public class PositNegatZero {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+        
+        // Verifica se o número é positivo, negativo ou igual a zero
+        if (numero > 0) {
+            System.out.println("O número é positivo.");
+        } else if (numero < 0) {
+            System.out.println("O número é negativo.");
+        } else {
+            System.out.println("O número é igual a zero.");
+        }
+    }
 
-	}
+
 
 }
